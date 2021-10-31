06830bdf9d21071e487f4c3436443ac22fc5a0f5
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-31 22:32:26 +0530

    Update global_stats.py

--- wbb/modules/global_stats.py
+++ wbb/modules/global_stats.py
@@ -102,8 +102,8 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
-    rurl = "https://github.com/thehamkercat/williambutcherbot"
+    url = "https://github.com/didikteatochhawng/LynnRoBot"
+    rurl = "https://github.com/didikteatochhawng/LynnRoBot"
     developers = await get(url)
     commits = 0
     for developer in developers:
